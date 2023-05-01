c5a6ebe40ca40b15f17272ca2e66ba076ae7f4db
illustratedattempts <vothanh3117@gmail.com>
2023-05-01 13:58:33 -0500

    Deprecated
    Streamlit is not really compatible with my requirements. Other possibility that I didn't try was just looping it. Although I am unsure how it would interact with Streamlit in terms of generating graphics.

--- gui_main2.py
+++ gui_main2.py
@@ -3,6 +3,10 @@ from streamlit_chat import message
 import sys
 import random
 import re
+import html
+
+from chatbot.YoutubeToolkit import YoutubeToolkit
+from urllib.parse import urlparse
 
 # Check if this is a NEW run
 if 'state' not in st.session_state:
@@ -12,7 +16,12 @@ if 'state' not in st.session_state:
     st.session_state['user'] = None
     st.session_state['enter_prompt'] = False
     st.session_state['testing_topic'] = None
+    st.session_state['chosen_video_title'] = None
+    st.session_state['chosen_video_link'] = None
     st.session_state['comment_not_enabled_prompt'] = False
+    st.session_state['verify_prompt'] = False
+    st.session_state['Youtube_Obj'] = YoutubeToolkit()
+
 
 
 def generate_random_key():
@@ -90,10 +99,11 @@ match st.session_state['state'][-1]:
             st.session_state['state'].append('Topic_Provided')
             st.experimental_rerun()
     case 'Link_Provided':
-        if re.search(
-                '(https:\/\/)?(www\.)?youtube\.com\/watch\?v=[a-zA-Z0-9\-\_]{11}$|(https:\/\/)?(www\.)?youtu\.be\/[a-zA-Z0-9\-\_]{11}',
-                st.session_state['testing_topic']):
-            if not False:  # @TODO Implement to see if comments enabled
+        youtube = st.session_state['Youtube_Obj']
+        topic = st.session_state['testing_topic']
+
+        if youtube.verify_url(topic):
+            if not youtube.verify_comments_enabled(youtube.get_video_id(urlparse(topic))):  # @TODO Implement to see if comments enabled
                 if not st.session_state['comment_not_enabled_prompt']:
                     display_robot_msg(
                         "It seems like the video you have chosen has comment scraping disabled. Please pick another.")
@@ -105,13 +115,68 @@ match st.session_state['state'][-1]:
                 st.session_state['state'].append('Link_OR_Topic')
                 st.experimental_rerun()
             else:
-                st.session_state['state'].append('Topic_Chosen')
+                st.session_state['chosen_video_link'] = topic  # Topic is presumed to be a valid link at this point
+                st.session_state['chosen_video_title'] = youtube.get_video_name(topic)
+                st.session_state['state'].append('Topic_Verification')
                 st.experimental_rerun()
     case 'Topic_Provided':
-        display_robot_msg("Please Pick a Video From The Following Listed Below")
+        # Just so I have to write less
+        youtube = st.session_state['Youtube_Obj']
+        topic = st.session_state['testing_topic']
+        videos_link_arr, videos_title_arr = youtube.get_topic_list(topic)
 
+        display_robot_msg("Please Pick a Video From The Following Listed Below")
+        videos_list = ''
+        for num in range(len(videos_title_arr)):
+            videos_list += str(num+1) + '. ' + html.unescape(videos_title_arr[num]) + ' | ' + videos_link_arr[num] + '\n'
+        display_robot_msg(videos_list)
+        display_robot_msg("Please enter the video integer number.")
+        st.session_state['state'].append('Choosing_Topic_Num')
+        st.experimental_rerun()
     # @TODO Checking the inputs etc etc
+    case 'Choosing_Topic_Num':
+        youtube = st.session_state['Youtube_Obj']
+        topic = st.session_state['testing_topic']
+        videos_link_arr, videos_title_arr = youtube.get_topic_list(topic)
+
+        selected_vid_index = user_text()
+        if selected_vid_index.isdigit():
+            selected_vid_index = int(selected_vid_index)
+            if 0 < selected_vid_index < 6:
+                selected_vid_index = selected_vid_index - 1
+                if not youtube.verify_comments_enabled(youtube.get_video_id(urlparse(videos_link_arr[selected_vid_index]))):
+                    display_robot_msg("It seems like the video you've chosen has comment scraping disabled. Please pick another topic.")
+                    st.session_state['state'].append('Topic_Provided')
+                    st.experimental_rerun()
+                else:
+                    st.session_state['chosen_video_link'] = videos_link_arr[selected_vid_index]
+                    st.session_state['chosen_video_title'] = videos_title_arr[selected_vid_index]
+                    st.session_state['state'].append('Topic_Verification')
+                    st.experimental_rerun()
+            else:
+                display_robot_msg("Please enter a listed video number.")
+                st.session_state['state'].append('Topic_Provided')
+                st.experimental_rerun()
+        else:
+            display_robot_msg("Please enter an INTEGER number.")
+            st.session_state['state'].append('Topic_Provided')
+            st.experimental_rerun()
+
+    case 'Topic_Verification':
+        if not st.session_state['verify_prompt']:
+            display_robot_msg("Are you sure you want to look at {}? (Y/N)".format(html.unescape(st.session_state['chosen_video_title'])))
+            st.session_state['verify_prompt'] = True
+        confirm_text = user_text()
+        confirm_text = confirm_text.lower()
+        st.session_state['verify_prompt'] = False
+        if confirm_text == 'y' or confirm_text == 'yes':
+            st.session_state['state'].append('Topic_Chosen')
+        elif confirm_text == 'n' or confirm_text == 'no':
+            display_robot_msg("Alright. Let us try again.")
+            st.session_state['state'].append('Topic_Selection')
+        else:
+            display_robot_msg("Please enter either YES(Y) or NO(N).")
+        st.experimental_rerun()
 
     case 'Topic_Chosen':
-        print("THIS NEVER RUNS! GOTTA DELETE LATER!")
-
+        print("WE GOT HERE!!!!")
